e5042056ee6a2a3b44a90498ac8a886b156202a6
Elena Stepanova <elenst@montyprogram.com>
2018-01-14 23:54:13 +0200

    Command line itself is printed on the next line

--- scripts/collect_failure_info.sh
+++ scripts/collect_failure_info.sh
@@ -28,7 +28,7 @@ do
 
   trial=`echo $triallog | sed -e 's/.*trial\([0-9]*\).*/\1/'`
   status=`grep 'will exit with exit status' $triallog | sed -e 's/.*will exit with exit status STATUS_\([A-Z_]*\).*/\1/'`
-  cmd=`grep 'Final command line:' $triallog`
+  cmd=`grep -A 1 'Final command line:' $triallog`
 
   echo "=================== Trial $trial ==================="
   echo
