1919ebae0de186babf4637ed768c5092cac67318
Elena Stepanova <elenst@montyprogram.com>
2018-02-01 17:26:06 +0200

    More known bugs

--- scripts/check_for_known_bugs.pl
+++ scripts/check_for_known_bugs.pl
@@ -28,24 +28,34 @@ while (<DATA>) {
 
 __DATA__
 
-MDEV-14864: in mysql_prepare_create_table
-MDEV-14833: [Draft] Failing assertion: trx->error_state == DB_SUCCESS in file /home/travis/src/storage/innobase/que/que0que.cc line [0-9]+
-MDEV-13103: fil0pagecompress.cc:[0-9]+: void fil_decompress_page
-MDEV-14829: protocol.cc:587: void Protocol::end_statement
-MDEV-15036: sql_error.cc:335: void Diagnostics_area::set_ok_status
-MDEV-14695: n < m_size
-MDEV-13699: == new_field->field_name.length
-MDEV-15103: virtual ha_rows ha_partition::part_records
-MDEV-14715: table->read_set, field_index
-MDEV-14995: in ha_partition::update_create_info
-MDEV-14932: in ha_partition::update_create_info
-MDEV-14762: has_stronger_or_equal_type
 MDEV-10130: share->in_trans == 0
+MDEV-12466: Open_tables_state::BACKUPS_AVAIL
+MDEV-13024: in multi_delete::send_data
+MDEV-13103: fil0pagecompress.cc:[0-9]+: void fil_decompress_page
+MDEV-13553: Open_tables_state::BACKUPS_AVAIL
+MDEV-13699: == new_field->field_name.length
+MDEV-14407: trx_undo_rec_copy
+MDEV-14695: n < m_size
+MDEV-14697: in TABLE::mark_default_fields_for_write
+MDEV-14715: table->read_set, field_index
+MDEV-14762: has_stronger_or_equal_type
 MDEV-14825: col->ord_part
+MDEV-14829: protocol.cc:587: void Protocol::end_statement
+MDEV-14831: Can't find record in 'seq[0-9]
+MDEV-14833: [Draft] Failing assertion: trx->error_state == DB_SUCCESS in file /home/travis/src/storage/innobase/que/que0que.cc line [0-9]+
+MDEV-14862: in add_key_equal_fields
+MDEV-14864: in mysql_prepare_create_table
+MDEV-14905: purge_sys->state == PURGE_STATE_INIT
+MDEV-14932: in ha_partition::update_create_info
 MDEV-14994: join->best_read < double
+MDEV-14995: in ha_partition::update_create_info
+MDEV-15036: sql_error.cc:335: void Diagnostics_area::set_ok_status
+MDEV-15103: virtual ha_rows ha_partition::part_records
 MDEV-15130: table->s->null_bytes == 0
+MDEV-15130: static void PFS_engine_table::set_field_char_utf8
+MDEV-15115: dict_tf2_is_valid
 MDEV-15117: in is_temporary_table
 MDEV-15149: table_share->tmp_table != NO_TMP_TABLE
 MDEV-15149: table->in_use == _current_thd
 MDEV-15149: tables->table->pos_in_table_list == tables
-MDEV-14407: trx_undo_rec_copy
+MDEV-15167: in THD::binlog_write_row
