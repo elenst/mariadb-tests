983eb8e6cc87d72d7b620b40b379deea7bfc45f8
Elena Stepanova <elenst@montyprogram.com>
2018-08-25 00:12:11 +0300

    Temporarily disable writing to the database

--- scripts/collect_single_failure_info.sh
+++ scripts/collect_single_failure_info.sh
@@ -57,7 +57,7 @@ function soft_exit
 
 function insert_success
 {
-   $MYSQL --host=$DB_HOST --port=$DB_PORT -u$DB_USER -p$DBP -e "REPLACE INTO travis.success SET build_id = $TRAVIS_BUILD_NUMBER, job_id = $TRAVIS_JOB, trial_id = $TRIAL, travis_branch = \"$TRAVIS_BRANCH\", result = \"$TRIAL_RESULT\", status = \"$TRIAL_STATUS\", command_line = \"$TRIAL_CMD\", server_branch = \"$SERVER\", server_revision = \"$REVISION\", cmake_options = \"$CMAKE_OPTIONS\", test_branch = \"$TEST_BRANCH\", test_revision = \"$TEST_REVISION\""
+#   $MYSQL --host=$DB_HOST --port=$DB_PORT -u$DB_USER -p$DBP -e "REPLACE INTO travis.success SET build_id = $TRAVIS_BUILD_NUMBER, job_id = $TRAVIS_JOB, trial_id = $TRIAL, travis_branch = \"$TRAVIS_BRANCH\", result = \"$TRIAL_RESULT\", status = \"$TRIAL_STATUS\", command_line = \"$TRIAL_CMD\", server_branch = \"$SERVER\", server_revision = \"$REVISION\", cmake_options = \"$CMAKE_OPTIONS\", test_branch = \"$TEST_BRANCH\", test_revision = \"$TEST_REVISION\""
 
   if [ "$?" != "0" ] ; then
     echo "ERROR: Failed to insert the successful result $TRIAL_RESULT for build_id = $TRAVIS_BUILD_NUMBER, job_id = $TRAVIS_JOB, trial_id = $TRIAL"
@@ -71,7 +71,7 @@ function load_failure
 {
   ls -l ${LOGDIR}/${ARCHDIR}.tar.gz
 
-  $MYSQL --local-infile --host=$DB_HOST --port=$DB_PORT -u$DB_USER -p$DBP -e "LOAD DATA LOCAL INFILE \"${LOGDIR}/${ARCHDIR}.tar.gz\" REPLACE INTO TABLE travis.failure CHARACTER SET BINARY FIELDS TERMINATED BY 'xxxxxthisxxlinexxxshouldxxneverxxeverxxappearxxinxxanyxxfilexxxxxxxxxxxxxxxxxxxxxxxx' ESCAPED BY '' LINES TERMINATED BY 'XXXTHISXXLINEXXSHOULDXXNEVERXXEVERXXAPPEARXXINXXANYXXFILEXXXXXXXXXXXXXXXXXXXX' (data) SET build_id = $TRAVIS_BUILD_NUMBER, job_id = $TRAVIS_JOB, trial_id = $TRIAL, travis_branch = \"$TRAVIS_BRANCH\", result = \"$TRIAL_RESULT\", status = \"$TRIAL_STATUS\", command_line = \"$TRIAL_CMD\", server_branch = \"$SERVER\", server_revision = \"$REVISION\", cmake_options = \"$CMAKE_OPTIONS\", test_branch = \"$TEST_BRANCH\", test_revision = \"$TEST_REVISION\""
+#  $MYSQL --local-infile --host=$DB_HOST --port=$DB_PORT -u$DB_USER -p$DBP -e "LOAD DATA LOCAL INFILE \"${LOGDIR}/${ARCHDIR}.tar.gz\" REPLACE INTO TABLE travis.failure CHARACTER SET BINARY FIELDS TERMINATED BY 'xxxxxthisxxlinexxxshouldxxneverxxeverxxappearxxinxxanyxxfilexxxxxxxxxxxxxxxxxxxxxxxx' ESCAPED BY '' LINES TERMINATED BY 'XXXTHISXXLINEXXSHOULDXXNEVERXXEVERXXAPPEARXXINXXANYXXFILEXXXXXXXXXXXXXXXXXXXX' (data) SET build_id = $TRAVIS_BUILD_NUMBER, job_id = $TRAVIS_JOB, trial_id = $TRIAL, travis_branch = \"$TRAVIS_BRANCH\", result = \"$TRIAL_RESULT\", status = \"$TRIAL_STATUS\", command_line = \"$TRIAL_CMD\", server_branch = \"$SERVER\", server_revision = \"$REVISION\", cmake_options = \"$CMAKE_OPTIONS\", test_branch = \"$TEST_BRANCH\", test_revision = \"$TEST_REVISION\""
 
   if [ "$?" != "0" ] ; then
     echo "ERROR: Failed to insert the failure $TRIAL_RESULT and load ${LOGDIR}/${ARCHDIR}.tar.gz for build_id = $TRAVIS_BUILD_NUMBER, job_id = $TRAVIS_JOB, trial_id = $TRIAL"
